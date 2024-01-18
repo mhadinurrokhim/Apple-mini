2d0843f79b2a0bd7b53cc0b9cc8f0b1b1a3c2bed
aisyeee <aisyeee2006@gmail.com>
2024-01-18 14:49:49 +0700

    p

--- resources/views/user/homeuser.blade.php
+++ resources/views/user/homeuser.blade.php
@@ -647,34 +647,37 @@
                      data-swiper='{"slidesPerView":1,"spaceBetween":16,"breakpoints":{"450":{"slidesPerView":2,"spaceBetween":16},"768":{"slidesPerView":3,"spaceBetween":20},"1200":{"slidesPerView":4,"spaceBetween":16},"1540":{"slidesPerView":5,"spaceBetween":16}}}'>
                   <div class="swiper-wrapper">
                     @foreach ($produk as $product)
-                      <div class="swiper-slide">
-                        <div class="position-relative text-decoration-none product-card h-100">
-                          <div class="d-flex flex-column justify-content-between h-100">
-                            <div>
-                              <div class="border border-1 rounded-3 position-relative mb-3">
-                                <button class="btn rounded-circle p-0 d-flex flex-center btn-wish z-index-2 d-toggle-container btn-outline-primary"
-                                        data-bs-toggle="tooltip" data-bs-placement="top" title="Add to wishlist">
-                                  <span class="fas fa-heart d-block-hover"></span>
-                                  <span class="far fa-heart d-none-hover"></span>
-                                </button>
-                                <img class="img-fluid" src="{{ asset('storage/Product/'. $product->path_produk) }}" alt="" style="width: 300px; height: 200px;" />
+                    <div class="swiper-slide">
+                        <div class="card">
+                          <div class="position-relative text-decoration-none product-card h-100">
+                            <div class="d-flex flex-column justify-content-between h-100">
+                              <div>
+                                <div class="border border-1 rounded-3 position-relative mb-3">
+                                  <button class="btn rounded-circle p-0 d-flex flex-center btn-wish z-index-2 d-toggle-container btn-outline-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Add to wishlist">
+                                    <span class="fas fa-heart d-block-hover"></span>
+                                    <span class="far fa-heart d-none-hover"></span>
+                                  </button>
+                                  <img class="card-img-top img-fluid" src="{{ asset('storage/Product/'. $product->path_produk) }}" alt="{{ $product->nama_produk }}" style="width: 300px; height: 200px;" />
+                                </div>
+                                <div class="card-body">
+                                  <a href="produkdetail" class="stretched-link">
+                                    <h6 class="card-title mb-2 lh-sm line-clamp-3 product-name">{{ $product->nama_produk }}</h6>
+                                </a>
+                                <p class="fs--1 text-1000 fw-bold mb-2">Stock {{ $product->stok }}</p>
+                                  <p class="fs--1">
+                                    <span class="fa fa-star text-warning"></span>
+                                    <span class="fa fa-star text-warning"></span>
+                                    <span class="fa fa-star text-warning"></span>
+                                    <span class="fa fa-star text-warning"></span>
+                                    <span class="fa fa-star text-warning"></span>
+                                    <span class="text-500 fw-semi-bold ms-1">(67 people rated)</span>
+                                  </p>
+                                </div>
                               </div>
-                              <a class="stretched-link" href="product-details.html">
-                                <h6 class="mb-2 lh-sm line-clamp-3 product-name">{{ $product->nama_produk }}</h6>
-                              </a>
-                              <p class="fs--1">
-                                <span class="fa fa-star text-warning"></span>
-                                <span class="fa fa-star text-warning"></span>
-                                <span class="fa fa-star text-warning"></span>
-                                <span class="fa fa-star text-warning"></span>
-                                <span class="fa fa-star text-warning"></span>
-                                <span class="text-500 fw-semi-bold ms-1">(67 people rated)</span>
-                              </p>
-                            </div>
-                            <div>
-                              <p class="fs--1 text-1000 fw-bold mb-2">Stock {{ $product->stok }}</p>
-                              <div class="d-flex align-items-center mb-1">
-                                <h3 class="text-1100 mb-0">Rp. 5000</h3>
+                              <div class="card-footer">
+                                <div class="d-flex align-items-center mb-1">
+                                  <h3 class="text-1100 mb-0">Rp {{ number_format($product->harga, 0, ',', '.') }}</h3>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -684,7 +687,6 @@
                   </div>
                 </div>
               </div>
-
 
     </section><!-- <section> close ============================-->
     <!-- ============================================-->
