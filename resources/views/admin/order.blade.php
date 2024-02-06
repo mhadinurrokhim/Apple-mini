ae6052533647cce256b6d15f6c2d38a2fddb9f25
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-02-06 19:29:34 +0700

    fix modal sesuai id

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -413,7 +413,7 @@
                                                               </div>
                                                         @elseif ($orders->status == 'reject')
                                                         <button class="btn btn-link" data-bs-toggle="modal" data-bs-target="#show-reject-message{{ $orders->id }}">Reject Message</button>
-                                                        <div class="modal fade" id="show-reject-message" tabindex="-1" aria-labelledby="rejectMessageModalLabel" aria-hidden="true">
+                                                        <div class="modal fade" id="show-reject-message{{ $orders->id }}" tabindex="-1" aria-labelledby="rejectMessageModalLabel" aria-hidden="true">
                                                             <div class="modal-dialog modal-dialog-centered">
                                                                 <div class="modal-content">
                                                                     <div class="modal-header">
@@ -421,7 +421,7 @@
                                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                     </div>
                                                                     <div class="modal-body">
-                                                                        <div class="w-100 fs-2">{{ $orders->reject_message }}</div>
+                                                                        <div class="w-100 fs-2"><p style="overflow-wrap: break-word;">{{ $orders->reject_message }}</p></div>
                                                                     </div>
                                                                     <div class="modal-footer">
                                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
